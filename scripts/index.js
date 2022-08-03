8951ca804fdf2da2a53f7f100274ae6254a53d3c
Jorge Diaz <emir.diazroman@gmail.com>
2022-08-02 23:21:34 -0500

    style: adecuate codde formatting

--- scripts/index.js
+++ scripts/index.js
@@ -12,25 +12,25 @@ let formName = document.querySelector(".form__name");
 
 let formJob = document.querySelector(".form__profession");
 
-editButton.addEventListener("click", function() {
-    popup.classList.toggle("popup_opened");
-    formName.value = profileName.textContent;
-    formJob.value = profileJob.textContent;
+editButton.addEventListener("click", function () {
+  popup.classList.toggle("popup_opened");
+  formName.value = profileName.textContent;
+  formJob.value = profileJob.textContent;
 });
 
 //Cerrar formulario
 const closeButton = document.querySelector(".close-btn");
 
-closeButton.addEventListener("click", function() {
-    popup.classList.toggle("popup_opened");
+closeButton.addEventListener("click", function () {
+  popup.classList.toggle("popup_opened");
 });
 
 //Cambiar la info del perfil al dar "Guardar"
 const editForm = document.querySelector(".form");
 
-editForm.addEventListener("submit", function(evt) {
-    evt.preventDefault();
-    profileName.textContent = formName.value;
-    profileJob.textContent = formJob.value;
-    popup.classList.toggle("popup_opened");
-})
+editForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  profileName.textContent = formName.value;
+  profileJob.textContent = formJob.value;
+  popup.classList.toggle("popup_opened");
+});
