238a9e3ac8e0fb2a1ddc5ed16b3123485bbcaafe
Jorge Diaz <emir.diazroman@gmail.com>
2022-08-17 22:41:51 -0500

    feat: create blocks, elements and style for add-form, rename classes for edit-form

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,53 @@
-//Abrir formulario y cargar los campos con los datos del perfil
-
+//Abrir formulario edit y cargar los campos con los datos del perfil
 const editButton = document.querySelector(".edit-btn");
-
-const popup = document.querySelector(".popup");
-
+const editPopup = document.querySelector(".edit-popup");
 const profileName = document.querySelector(".profile__name");
-
 const profileJob = document.querySelector(".profile__profession");
-
-const formName = document.querySelector(".form__name");
-
-const formJob = document.querySelector(".form__profession");
+const formName = document.querySelector(".edit-form__name");
+const formJob = document.querySelector(".edit-form__profession");
 
 editButton.addEventListener("click", function () {
-  popup.classList.toggle("popup_opened");
+  editPopup.classList.toggle("edit-popup_opened");
+  document.querySelector(".edit-popup_opened").style.visibility = "visible";
+  document.querySelector(".edit-popup_opened").style.opacity = "1";
   formName.value = profileName.textContent;
   formJob.value = profileJob.textContent;
 });
 
-//Cerrar formulario
-const closeButton = document.querySelector(".close-btn");
+//Cerrar formulario edit
+const editCloseButton = editPopup.querySelector(".close-btn");
 
-closeButton.addEventListener("click", function () {
-  popup.classList.toggle("popup_opened");
+editCloseButton.addEventListener("click", function () {
+  editPopup.classList.toggle("edit-popup_opened");
 });
 
 //Cambiar la info del perfil al dar "Guardar"
-const editForm = document.querySelector(".form");
+const editForm = document.querySelector(".edit-form");
 
 editForm.addEventListener("submit", function (evt) {
   evt.preventDefault();
   profileName.textContent = formName.value;
   profileJob.textContent = formJob.value;
-  popup.classList.toggle("popup_opened");
+  editPopup.classList.toggle("edit-popup_opened");
+});
+
+//Abrir formulario agregar
+const addButton = document.querySelector(".add-btn");
+const addPopup = document.querySelector(".add-popup");
+const cardName = document.querySelector(".card__name");
+const cardLink = document.querySelector(".card__image");
+const formPlace = document.querySelector(".add-form__place");
+const formLink = document.querySelector(".add-form__link");
+
+addButton.addEventListener("click", function () {
+  addPopup.classList.toggle("add-popup_opened");
+  //formPlace.value = cardName.textContent;
+  //formLink.value = cardLink.textContent;
+});
+
+//Cerrar formulario agregar
+const addCloseButton = addPopup.querySelector(".close-btn");
+
+addCloseButton.addEventListener("click", function () {
+  addPopup.classList.toggle("add-popup_opened");
 });
